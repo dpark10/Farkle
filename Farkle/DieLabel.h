5ab926f67513250c090db25a32bde68a742110a2
Matthew Bracamonte <MatthewBraca@Matthews-MacBook-Pro.local>
2016-03-24 14:22:28 -0500

    set up storyboard

---
+++ Farkle/DieLabel.h
@@ -0,0 +1,13 @@
+//
+//  DieLabel.h
+//  Farkle
+//
+//  Created by Matthew Bracamonte on 3/24/16.
+//  Copyright © 2016 Dan Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DieLabel : UILabel
+
+@end
---
+++ Farkle/DieLabel.m
@@ -0,0 +1,15 @@
+//
+//  DieLabel.m
+//  Farkle
+//
+//  Created by Matthew Bracamonte on 3/24/16.
+//  Copyright © 2016 Dan Park. All rights reserved.
+//
+
+#import "DieLabel.h"
+
+@implementation DieLabel
+
+
+
+@end
